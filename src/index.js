6d954ace3d6c53d50085722433b30e17fbe6f61a
FedirAnichkin <anichkin.fedir@gmail.com>
2019-04-30 20:12:47 +0300

    commit 2nd step Brain-games

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const askUserName = () => {
+    const askName = readlineSync.question('May I have your name? ');
+    return console.log('Hello, ' + askName + '!');
+};
+export default askUserName;
