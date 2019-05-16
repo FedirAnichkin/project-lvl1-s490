eb970f2424b9422bc4bc4f420aa0f60e2401a01d
FedirAnichkin <anichkin.fedir@gmail.com>
2019-05-16 21:20:49 +0300

    modify in gameEngine

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import readlineSync from 'readline-sync';
 
+const gameRounds = 3;
+
 const gameEngine = (message, func) => {
   console.log('Welcome to Brain Games!');
   console.log(message);
   const askName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello ${askName}!\n`);
-  const gameRounds = 3;
   for (let i = 1; i <= gameRounds; i += 1) {
     const [rightAnswer, task] = func();
     console.log(`Question: ${task}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const writeAnswer = Number(userAnswer) ? parseFloat(userAnswer) : userAnswer.toLowerCase();
-    if (rightAnswer === writeAnswer) {
+    if (rightAnswer === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`\n'${writeAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`\n'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${askName}.`);
       return;
     }
