f8a3834c45094010a31bf0f1c511231817276178
FedirAnichkin <anichkin.fedir@gmail.com>
2019-05-16 21:23:28 +0300

    modify function progressionNumber

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,19 @@
 import gameEngine from '..';
 import randomNumber from '../utils';
 
+const progressionLength = 10;
+
 const greeting = 'What number is missing in the progression?';
 const progressionNumber = () => {
-  const progressionLength = 10;
-  const progressionArray = [];
   const step = randomNumber(2, 7);
   const firstElement = randomNumber(1, 10);
   const hiddenElementPosition = randomNumber(0, progressionLength - 1);
   let message = '';
   for (let i = 0; i < progressionLength; i += 1) {
-    const n = i === 0 ? firstElement : firstElement + step * i;
-    progressionArray[i] = n;
-    message += (i === hiddenElementPosition) ? '.. ' : `${n} `;
+    message += (i === hiddenElementPosition) ? '.. ' : `${firstElement + i * step} `;
   }
-  const task = message;
-  const rightAnswer = progressionArray[hiddenElementPosition];
+  const task = message.trim();
+  const rightAnswer = String(firstElement + hiddenElementPosition * step);
   return [rightAnswer, task];
 };
 export default () => gameEngine(greeting, progressionNumber);
